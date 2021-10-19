fb4d0e0d04b1b53a5e2384db47d95da8bd782274
Awatif Ismail <awatif3514@gmail.com>
2021-10-20 04:57:21 +0800

    Added GetBooking()

--- tableapp/js/list.js
+++ tableapp/js/list.js
@@ -1,5 +1,5 @@
 let refreshNowBtn = document.getElementById("refreshNow")
-refreshNowBtn.addEventListener("click", function() {
+refreshNowBtn.addEventListener("click", function () {
     GetBooking()
 })
 
@@ -14,26 +14,26 @@ function GetBooking() {
             let bookingNameList = document.getElementById("bookingNameList")
 
             //delete all row in the table
-            for(let k = bookingNameList.rows.length - 1; k > 0; k--){
+            for (let k = bookingNameList.rows.length - 1; k > 0; k--) {
                 bookingNameList.deleteRow(k)
             }
 
             //load all rows from Sheety API
-            for(let i = 0; i < json.bookings.length; i++){
-            let gName = json.bookings[i].name;
-            let gEmail = json.bookings[i].email;
-            let gPax = json.bookings[i].pax;
-            let gRemarks = json.bookings[i].remarks;
-            let gId = json.bookings[i].id;
+            for (let i = 0; i < json.bookings.length; i++) {
+                let gName = json.bookings[i].name;
+                let gEmail = json.bookings[i].email;
+                let gPax = json.bookings[i].pax;
+                let gRemarks = json.bookings[i].remarks;
+                let gId = json.bookings[i].id;
 
-            let row = bookingNameList.insertRow(bookingNameList.rows.length)
-            row.insertCell(0).innerHTML = gId
-            row.insertCell(1).innerHTML = gName
-            row.insertCell(2).innerHTML = gEmail
-            row.insertCell(3).innerHTML = gPax
-            row.insertCell(4).innerHTML = gRemarks
-            row.insertCell(5).innerHTML = ""
-        
+                let row = bookingNameList.insertRow(bookingNameList.rows.length)
+                row.insertCell(0).innerHTML = gId
+                row.insertCell(1).innerHTML = gName
+                row.insertCell(2).innerHTML = gEmail
+                row.insertCell(3).innerHTML = gPax
+                row.insertCell(4).innerHTML = gRemarks
+                row.insertCell(5).innerHTML = ""
+
             }
         });
 }
