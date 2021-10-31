600b05eb33f40e8e466e3675ac13532cb693a5c5
Awatif Ismail <awatif3514@gmail.com>
2021-10-31 18:41:16 +0800

    edit Q3 js

--- Assignment04/js/Q3.js
+++ Assignment04/js/Q3.js
@@ -1,18 +1,14 @@
 function factorial(n){
     
-    if(n == 1){
+    if(nombor == 1){
         return 1;
     
     }else{
-        return n * factorial(n-1) ;
+        return nombor * factorial(n-1) ;
     }
 }
 let elfactorial = document.getElementById("nombor")
 elfactorial.addEventListener("click", function(){
-    factorial(n)
-    console.log("The factorial of " + n + " is " + factorial);
-
-
-
-
-
+    
+    console.log("The factorial of " + nombor + " is " + factorial);
+})
